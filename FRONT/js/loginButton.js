5b23c3510b7924b4379d6c945e28a74eb8fa76a1
MaroBZ <136346712+MaroBZ@users.noreply.github.com>
2024-04-07 22:32:38 +0200

    Add files via upload
    bug fixed

--- FRONT/js/loginButton.js
+++ FRONT/js/loginButton.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.login-button').addEventListener('click', function() {
-        window.location.href = 'login.html';
+    // Query all elements with the class 'login-button'
+    var loginButtons = document.querySelectorAll('.login-button');
+
+    // Iterate through the NodeList and attach the event listener to each button
+    loginButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            window.location.href = 'login.html';
+        });
     });
 });
-
-
